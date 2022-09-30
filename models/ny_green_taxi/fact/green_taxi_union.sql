cd15bec6acd2a1051b39bf4fd783e8d29775652d
Fakhriansyah Wijaya <107629397+soldonmaybe@users.noreply.github.com>
2022-09-30 17:34:12 +0700

    Update green_taxi_union.sql

--- models/ny_green_taxi/fact/green_taxi_union.sql
+++ models/ny_green_taxi/fact/green_taxi_union.sql
@@ -2,24 +2,24 @@
 
 with january as 
 (
-  select ROW_NUMBER() OVER (ORDER BY lpep_pickup_datetime) as tripID,* 
+  select ROW_NUMBER() OVER (ORDER BY pickup_datetime) as tripID,* 
   from {{ ref('green_taxi_2021_01') }}
 ),
 february as
 (
-  select ROW_NUMBER() OVER (ORDER BY lpep_pickup_datetime) as tripID,* 
+  select ROW_NUMBER() OVER (ORDER BY pickup_datetime) as tripID,* 
   from {{ ref('green_taxi_2021_02') }}
 ),
 march as
 (
-  select ROW_NUMBER() OVER (ORDER BY lpep_pickup_datetime) as tripID,* 
+  select ROW_NUMBER() OVER (ORDER BY pickup_datetime) as tripID,* 
   from {{ ref('green_taxi_2021_03') }}
 )
 
 SELECT 
        tripID, 
-       lpep_pickup_datetime, 
-       lpep_dropoff_datetime, 
+       pickup_datetime, 
+       dropoff_datetime, 
        CAST(vendorID AS INT64) as vendorID,
        CAST(RatecodeID AS INT64) as RatecodeID,
        CAST(PULocationID AS INT64) as PULocationID,
@@ -28,8 +28,8 @@ FROM january
 UNION ALL
 SELECT 
        tripID, 
-       lpep_pickup_datetime, 
-       lpep_dropoff_datetime, 
+       pickup_datetime, 
+       dropoff_datetime, 
        CAST(vendorID AS INT64) as vendorID,
        CAST(RatecodeID AS INT64) as RatecodeID,
        CAST(PULocationID AS INT64) as PULocationID,
@@ -38,8 +38,8 @@ FROM february
 UNION ALL
 SELECT 
        tripID, 
-       lpep_pickup_datetime, 
-       lpep_dropoff_datetime, 
+       pickup_datetime, 
+       dropoff_datetime, 
        CAST(vendorID AS INT64) as vendorID,
        CAST(RatecodeID AS INT64) as RatecodeID,
        CAST(PULocationID AS INT64) as PULocationID,
